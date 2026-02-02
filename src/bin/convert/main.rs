3e705de580ee21442ca1e482e51e4a08609d1d9c
David Rasch <David.Rasch@cwi.nl>
2026-02-02 17:06:22 +0100

    import all tensors from unet, clip, autoencoder from safetensor file

--- src/bin/convert/main.rs
+++ src/bin/convert/main.rs
@@ -9,29 +9,23 @@ use burn::backend::ndarray::{NdArray, NdArrayDevice};
 use burn::store::{BurnpackStore, ModuleSnapshot, SafetensorsStore};
 use burn::tensor::backend::Backend;
 
-use burn_store::{ApplyResult, PyTorchToBurnAdapter};
-use stablediffusion::model::autoencoder::AutoencoderConfig;
-use stablediffusion::model::clip::CLIPConfig;
-use stablediffusion::model::stablediffusion::StableDiffusionConfig;
-use stablediffusion::model::unet::UNetConfig;
+use burn::store::{ApplyResult, KeyRemapper, PyTorchToBurnAdapter};
+use stablediffusion::model::stablediffusion::{StableDiffusion, StableDiffusionConfig};
 
-fn convert_safetensor_to_model<B: Backend>(
-    input_file: &str,
-    output_file: &str,
+fn load_pretrained<B: Backend>(
+    checkpoint: &str,
     device: &B::Device,
-) -> Result<(), Box<dyn Error>> {
+) -> Result<StableDiffusion<B>, Box<dyn Error>> {
     println!("Loading safetensor...");
-
-    //let clip_config = CLIPConfig::new(49408, 768, 12, 77, 12);
-    //let mut model = clip_config.init::<B>(device);
-
-    //let autoencoder_config = AutoencoderConfig::new();
-    //let mut model = autoencoder_config.init::<B>(device);
-    let unet_config = UNetConfig::new();
-    let mut model = unet_config.init::<B>(device);
-    let tensor_path = PathBuf::from(input_file);
-    let mut store = build_store(&tensor_path);
-
+    let key_mappings = key_remap_rules_sd();
+    let remapper = KeyRemapper::from_patterns(key_mappings).expect("Invalid key mapping regex");
+    let mut store = SafetensorsStore::from_file(checkpoint)
+        .with_from_adapter(PyTorchToBurnAdapter)
+        .remap(remapper)
+        .allow_partial(true)
+        .validate(true);
+    let sd_config = StableDiffusionConfig::default();
+    let mut model = sd_config.init::<B>(device);
     println!("Loading model");
     let result = model.load_from(&mut store);
     // TODO report
@@ -45,137 +39,156 @@ fn convert_safetensor_to_model<B: Backend>(
             unused,
             errors,
         }) => {
-            println!("decoder----");
             println!("missing: {:#?}", missing);
             //println!("unused: {:#?}",unused);
             println!("errors: {:#?}", errors);
-            println!("applied {:#?}", applied);
+            //println!("applied {:#?}", applied);
         }
         Err(e) => {
             println!("{:#?}", e);
         }
     }
-    /*
-    println!("Saving burnpack...");
-    let mut store = BurnpackStore::from_file(&output_file)
-        .overwrite(true)
-        .metadata("format", "safetensor")
-        .metadata("description", "Sample file for examining Burnpack format")
-        .metadata("version", env!("CARGO_PKG_VERSION"))
-        .metadata("author", "Burn Example");
-    model.save_into(&mut store).expect("Failed to save model");
-    */
-    Ok(())
+    Ok(model)
 }
 
-fn build_store(path: &Path) -> SafetensorsStore {
-    let mut store = SafetensorsStore::from_file(path);
-    for &(from, to) in key_remap_rules_unet() {
-        store = store.with_key_remapping(from, to);
-    }
-    store
-        .with_from_adapter(PyTorchToBurnAdapter)
-        .allow_partial(true)
-        .validate(true)
+fn key_remap_rules_sd() -> Vec<(String, String)> {
+    let mut rules: Vec<(String, String)> = Vec::new();
+    key_remap_rules_clip()
+        .iter()
+        .for_each(|(a, b)| rules.push((format!("{}", a), format!("clip.{}", b))));
+    key_remap_rules_autoencoder()
+        .iter()
+        .for_each(|(a, b)| rules.push((format!("{}", a), format!("autoencoder.{}", b))));
+    key_remap_rules_unet()
+        .iter()
+        .for_each(|(a, b)| rules.push((format!("{}", a), format!("{}", b))));
+    rules.push((
+        format!("alphas_cumprod"),
+        format!("alpha_cumulative_products"),
+    ));
+    rules
 }
 fn key_remap_rules_unet() -> &'static [(&'static str, &'static str)] {
     &[
-        (r"model\.diffusion_model\.time_embed.0", "lin1_time_embed"),
-        (r"model\.diffusion_model\.time_embed.2", "lin2_time_embed"),
-        (r"model\.diffusion_model\.out.0.weight", "norm_out.gamma"),
-        (r"model\.diffusion_model\.out.0.bias", "norm_out.beta"),
-        (r"model\.diffusion_model\.out.2", "conv_out"),
-        // input blocks
         (
-            r"model\.diffusion_model\.input_blocks\.0\.0",
-            "input_blocks.conv",
-        ),
-        //(r"model\.diffusion_model\.input_blocks\.1","input_blocks.rt1"),
-        //(r"model\.diffusion_model\.input_blocks\.1","input_blocks.rt1"),
-
-        //(r"model\.diffusion_model\.input_blocks\.2","input_blocks.rt2"),
-        //(r"model\.diffusion_model\.input_blocks\.3\.0","input_blocks.d1"),
-        // resblock 1.0 -> res
-        //model.diffusion_model.input_blocks.1.0.in_layers.0.bias
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.0\.in_layers\.0\.weight",
-            "input_blocks.rt1.res.norm_in.gamma",
+            r"model\.diffusion_model\.time_embed\.0",
+            "diffusion.lin1_time_embed",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.0\.in_layers\.0\.bias",
-            "input_blocks.rt1.res.norm_in.beta",
+            r"model\.diffusion_model\.time_embed\.2",
+            "diffusion.lin2_time_embed",
+        ),
+        (r"model\.diffusion_model\.out\.0", "diffusion.norm_out"),
+        (r"model\.diffusion_model\.out\.2", "diffusion.conv_out"),
+        (
+            r"model\.diffusion_model\.input_blocks",
+            "diffusion.input_blocks",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.0\.in_layers\.2",
-            "input_blocks.rt1.res.conv_in",
+            r"model\.diffusion_model\.output_blocks",
+            "diffusion.output_blocks",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.0\.emb_layers\.1\.(.*)",
-            "input_blocks.rt1.res.lin_embed.$1",
+            r"model\.diffusion_model\.middle_block",
+            "diffusion.middle_block",
+        ),
+        (r"input_blocks\.0\.0", "input_blocks.conv"),
+        (r"input_blocks\.1\.", "input_blocks.rt1."),
+        (r"input_blocks\.2\.", "input_blocks.rt2."),
+        (r"input_blocks\.3\.0.op", "input_blocks.d1"),
+        (r"input_blocks\.4\.", "input_blocks.rt3."),
+        (r"input_blocks\.5\.", "input_blocks.rt4."),
+        (r"input_blocks\.6\.0.op", "input_blocks.d2"),
+        (r"input_blocks\.7\.", "input_blocks.rt5."),
+        (r"input_blocks\.8\.", "input_blocks.rt6."),
+        (r"input_blocks\.9\.0.op", "input_blocks.d3"),
+        (r"input_blocks\.10\.", "input_blocks.r1."),
+        (r"input_blocks\.11\.", "input_blocks.r2."),
+        (r"output_blocks\.0\.", "output_blocks.r1."),
+        (r"output_blocks\.1\.", "output_blocks.r2."),
+        (r"output_blocks\.2\.", "output_blocks.ru."),
+        (r"output_blocks\.3\.", "output_blocks.rt1."),
+        (r"output_blocks\.4\.", "output_blocks.rt2."),
+        (r"output_blocks\.5\.", "output_blocks.rtu1."),
+        (r"output_blocks\.6\.", "output_blocks.rt3."),
+        (r"output_blocks\.7\.", "output_blocks.rt4."),
+        (r"output_blocks\.8\.", "output_blocks.rtu2."),
+        (r"output_blocks\.9\.", "output_blocks.rt5."),
+        (r"output_blocks\.10\.", "output_blocks.rt6."),
+        (r"output_blocks\.11\.", "output_blocks.rt7."),
+        (r"(rt\d+|ru|rtu\d+)\.0\.in_layers\.0", "$1.res.norm_in"),
+        (r"(rt\d+|ru|rtu\d+)\.0\.in_layers\.2", "$1.res.conv_in"),
+        (r"(rt\d+|ru|rtu\d+)\.0\.emb_layers\.1", "$1.res.lin_embed"),
+        (r"(rt\d+|ru|rtu\d+)\.0\.out_layers\.0", "$1.res.norm_out"),
+        (r"(rt\d+|ru|rtu\d+)\.0\.out_layers\.3", "$1.res.conv_out"),
+        (
+            r"(rt\d+|ru|rtu\d+)\.0\.skip_connection",
+            "$1.res.skip_connection",
+        ),
+        (r"(ru)\.1", "$1.upsample"),
+        (r"(rtu\d+)\.2", "$1.upsample"),
+        (r"(r\d+)\.0\.in_layers\.0", "$1.norm_in"),
+        (r"(r\d+)\.0\.in_layers\.2", "$1.conv_in"),
+        (r"(r\d+)\.0\.emb_layers\.1", "$1.lin_embed"),
+        (r"(r\d+)\.0\.out_layers\.0", "$1.norm_out"),
+        (r"(r\d+)\.0\.out_layers\.3", "$1.conv_out"),
+        (r"(r\d+)\.0\.skip_connection", "$1.skip_connection"),
+        (
+            r"middle_block\.0\.in_layers\.0",
+            "middle_block.res1.norm_in",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.0\.out_layers\.0\.weight",
-            "input_blocks.rt1.res.norm_out.gamma",
+            r"middle_block\.0\.in_layers\.2",
+            "middle_block.res1.conv_in",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.0\.out_layers\.0\.bias",
-            "input_blocks.rt1.res.norm_out.beta",
+            r"middle_block\.0\.emb_layers\.1",
+            "middle_block.res1.lin_embed",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.0\.out_layers\.3",
-            "input_blocks.rt1.res.conv_out",
-        ),
-        //spatial transformer -> transformer
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.norm\.weight",
-            "input_blocks.rt1.transformer.norm.gamma",
+            r"middle_block\.0\.out_layers\.0",
+            "middle_block.res1.norm_out",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.norm\.bias",
-            "input_blocks.rt1.transformer.norm.beta",
+            r"middle_block\.0\.out_layers\.3",
+            "middle_block.res1.conv_out",
+        ),
+        (r"middle_block\.1", "middle_block.transformer"),
+        (
+            r"middle_block\.2\.in_layers\.0",
+            "middle_block.res2.norm_in",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.proj_in",
-            "input_blocks.rt1.transformer.proj_in",
+            r"middle_block\.2\.in_layers\.2",
+            "middle_block.res2.conv_in",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.proj_out",
-            "input_blocks.rt1.transformer.proj_out",
+            r"middle_block\.2\.emb_layers\.1",
+            "middle_block.res2.lin_embed",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.norm(\d+).weight",
-            "input_blocks.rt1.transformer.transformer.norm$1.gamma",
+            r"middle_block\.2\.out_layers\.0",
+            "middle_block.res2.norm_out",
         ),
         (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.norm(\d+).bias",
-            "input_blocks.rt1.transformer.transformer.norm$1.beta",
+            r"middle_block\.2\.out_layers\.3",
+            "middle_block.res2.conv_out",
         ),
+        // res transformer
+        (r"middle_block\.1", "$1.transformer"),
+        (r"(rt\d+|rtu\d+)\.1", "$1.transformer"),
+        (r"(.*?).transformer_blocks.0", "$1.transformer"),
         // cross attention
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.attn(\d+)\.to_q",
-            "input_blocks.rt1.transformer.transformer.attn$1.query",
-        ),
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.attn(\d+)\.to_k",
-            "input_blocks.rt1.transformer.transformer.attn$1.key",
-        ),
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.attn(\d+)\.to_v",
-            "input_blocks.rt1.transformer.transformer.attn$1.value",
-        ),
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.attn(\d+)\.to_out\.0",
-            "input_blocks.rt1.transformer.transformer.attn$1.out",
-        ),
+        (r"transformer.attn(\d+)\.to_q", "transformer.attn$1.query"),
+        (r"transformer.attn(\d+)\.to_k", "transformer.attn$1.key"),
+        (r"transformer.attn(\d+)\.to_v", "transformer.attn$1.value"),
+        (r"transformer.attn(\d+)\.to_out.0", "transformer.attn$1.out"),
         // feed forward
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.ff\.net.0.proj",
-            "input_blocks.rt1.transformer.transformer.mlp.geglu.proj",
-        ),
-        (
-            r"model\.diffusion_model\.input_blocks\.1\.1\.transformer_blocks\.0\.ff\.net.2",
-            "input_blocks.rt1.transformer.transformer.mlp.lin",
-        ),
+        (r"transformer.ff\.net.0.proj", "transformer.mlp.geglu.proj"),
+        (r"transformer.ff\.net.2", "transformer.mlp.lin"),
+        // convert norm weight -> norm gamma
+        ("norm(.*?).weight", "norm$1.gamma"),
+        ("norm(.*?).bias", "norm$1.beta"),
     ]
 }
 
@@ -252,31 +265,7 @@ fn key_remap_rules_autoencoder() -> &'static [(&'static str, &'static str)] {
             r"first_stage_model\.decoder\.up\.3\.upsample\.conv\.(.*)",
             "decoder.blocks.0.upsampler.$1",
         ),
-        (
-            r"first_stage_model\.decoder\.norm_out.weight",
-            "decoder.norm_out.gamma",
-        ),
-        (
-            r"first_stage_model\.decoder\.norm_out.bias",
-            "decoder.norm_out.beta",
-        ),
-        // fix up weights
-        (
-            r"decoder\.blocks\.(\d+)\.res(\d+).norm(\d+)\.weight",
-            "decoder.blocks.$1.res$2.norm$3.gamma",
-        ),
-        (
-            r"decoder\.blocks\.(\d+)\.res(\d+).norm(\d+)\.bias",
-            "decoder.blocks.$1.res$2.norm$3.beta",
-        ),
-        (
-            r"decoder\.mid\.block_(\d+)\.norm(\d+)\.bias",
-            "decoder.mid.block_$1.norm$2.beta",
-        ),
-        (
-            r"decoder\.mid\.block_(\d+)\.norm(\d+)\.weight",
-            "decoder.mid.block_$1.norm$2.gamma",
-        ),
+        (r"first_stage_model\.decoder\.norm_out", "decoder.norm_out"),
         // autoencoder: decoder.mid
         (
             r"first_stage_model\.decoder\.mid\.block_(1|2)\.(.*)",
@@ -287,12 +276,8 @@ fn key_remap_rules_autoencoder() -> &'static [(&'static str, &'static str)] {
             "decoder.mid.block_$1.$2",
         ),
         (
-            r"first_stage_model\.decoder\.mid\.attn_1\.norm\.bias",
-            "decoder.mid.attn.norm.beta",
-        ),
-        (
-            r"first_stage_model\.decoder\.mid\.attn_1\.norm\.weight",
-            "decoder.mid.attn.norm.gamma",
+            r"first_stage_model\.decoder\.mid\.attn_1\.norm",
+            "decoder.mid.attn.norm",
         ),
         (
             r"first_stage_model\.decoder\.mid\.attn_1\.(q|k|v|proj_out)",
@@ -313,6 +298,7 @@ fn key_remap_rules_autoencoder() -> &'static [(&'static str, &'static str)] {
             r"first_stage_model\.encoder\.down\.(\d+)\.downsample\.conv\.(.*)",
             "encoder.blocks.$1.downsampler.conv.$2",
         ),
+        (r"first_stage_model\.encoder\.norm_out", "encoder.norm_out"),
         // encoder mid
         (
             r"first_stage_model\.encoder\.mid\.block_(1|2)\.(.*)",
@@ -323,41 +309,12 @@ fn key_remap_rules_autoencoder() -> &'static [(&'static str, &'static str)] {
             "encoder.mid.block_$1.$2",
         ),
         (
-            r"first_stage_model\.encoder\.mid\.attn_1\.norm\.bias",
-            "encoder.mid.attn.norm.beta",
-        ),
-        (
-            r"first_stage_model\.encoder\.mid\.attn_1\.norm\.weight",
-            "encoder.mid.attn.norm.gamma",
+            r"first_stage_model\.encoder\.mid\.attn_1\.norm",
+            "encoder.mid.attn.norm",
         ),
         (
             r"first_stage_model\.encoder\.mid\.attn_1\.(q|k|v|proj_out)",
             "encoder.mid.attn.$1",
-        ),
-        (
-            r"first_stage_model\.encoder\.norm_out.weight",
-            "encoder.norm_out.gamma",
-        ),
-        (
-            r"first_stage_model\.encoder\.norm_out.bias",
-            "encoder.norm_out.beta",
-        ),
-        // fix up weights
-        (
-            r"encoder\.blocks\.(\d+)\.res(\d+).norm(\d+)\.weight",
-            "encoder.blocks.$1.res$2.norm$3.gamma",
-        ),
-        (
-            r"encoder\.blocks\.(\d+)\.res(\d+).norm(\d+)\.bias",
-            "encoder.blocks.$1.res$2.norm$3.beta",
-        ),
-        (
-            r"encoder\.mid\.block_(\d+)\.norm(\d+)\.bias",
-            "encoder.mid.block_$1.norm$2.beta",
-        ),
-        (
-            r"encoder\.mid\.block_(\d+)\.norm(\d+)\.weight",
-            "encoder.mid.block_$1.norm$2.gamma",
         ),
     ]
 }
@@ -426,17 +383,29 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
     if args.len() != 3 {
-        eprintln!("Usage: {} <dump_path> <model_name>", args[0]);
+        eprintln!("Usage: {} <checkpoint> <model_name>", args[0]);
         process::exit(1);
     }
 
-    let dump_path = &args[1];
+    let checkpoint = &args[1];
     let model_name = &args[2];
 
-    if let Err(e) = convert_safetensor_to_model::<Backend>(dump_path, model_name, &device) {
-        eprintln!("Failed to convert dump to model: {:?}", e);
-        process::exit(1);
-    }
+    match load_pretrained::<Backend>(checkpoint, &device) {
+        Ok(model) => {
+            println!("Saving burnpack...");
 
-    println!("Successfully converted {} to {}", dump_path, model_name);
+            let mut store = BurnpackStore::from_file(&model_name)
+                .overwrite(true)
+                .metadata("format", "safetensor")
+                .metadata("description", "Sample file for examining Burnpack format")
+                .metadata("version", env!("CARGO_PKG_VERSION"))
+                .metadata("author", "Burn Example");
+            model.save_into(&mut store).expect("Failed to save model");
+        }
+        Err(e) => {
+            eprintln!("Failed to convert dump to model: {:?}", e);
+            process::exit(1);
+        }
+    }
+    println!("Successfully converted {} to {}", checkpoint, model_name);
 }
